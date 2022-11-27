857bd350c7c07afa9c63f57a1a705a2f2cf25e9b
OngYiOngYi <you@example.com>
2022-11-27 17:27:52 +0800

    Added shows page, downloaded img for songs2.html

---
+++ script.js
@@ -0,0 +1,30 @@
+const countdown = document.querySelector(".countdown");
+
+const interval = setInterval(() => {
+  const deadline = new Date(2022, 11, 18, 13, 00, 00);
+
+  const current = new Date();
+
+  const diff = deadline - current;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24)) + "";
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + "";
+  const minutes = Math.floor((diff / (1000 * 60)) % 60) + "";
+  const seconds = Math.floor((diff / 1000) % 60) + "";
+
+  countdown.innerHTML = `
+    <div data-content="Days">${days.length === 1 ? `0${days}` : days}</div>
+    <div data-content="Hours">${hours.length === 1 ? `0${hours}` : hours}</div>
+    <div data-content="Minutes">${
+      minutes.length === 1 ? `0${minutes}` : minutes
+    }</div>
+    <div data-content="Seconds">${
+      seconds.length === 1 ? `0${seconds}` : seconds
+    }</div>
+`;
+
+  if (diff < 0) {
+    clearInterval(interval);
+    countdown.innerHTML = "<h1>Event Ended</h1>";
+  }
+}, 1000);
